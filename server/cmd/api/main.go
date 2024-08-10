67e41952b45fecbd80694367bdfbb1a642d8aedb
Syed <syshah@cloudpareto.com>
2024-08-09 23:31:20 -0400

    Add testify and mockery into project, init TDD on user service

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -44,7 +44,6 @@ func main() {
     router := http.NewServeMux()
     router.HandleFunc("/register", userHandler.RegisterUser)
     router.HandleFunc("/login", authHandler.Login)
-    // other routes...
 
     log.Println("Starting server on :8080...")
     if err := http.ListenAndServe(":8080", router); err != nil {
